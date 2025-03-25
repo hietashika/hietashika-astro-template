cd538a3251ec63cca7746060d4637ea87d0a29d0
hietashika <hietashika@gmail.com>
2025-03-25 20:48:54 +0900

    [feat] ✨ TypeScript設定とレイアウトの改善 - tsconfig.jsonにパスエイリアスを追加し、プロジェクトの構造を明確化 - VSCodeの推奨拡張機能を更新 - Layout.astroを日本語対応にし、ヘッダーとフッターのグリッドレイアウトを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,26 @@
+
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import mdx from "@astrojs/mdx";
+
+import playformCompress from "@playform/compress";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [mdx(), playformCompress()],
+	devToolbar: {
+		enabled: false,
+	},
+	vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: `
+					@use "@styles/tools/_mixin.scss" as *;
+					@use "@styles/tools/_function.scss" as *;`,
+				},
+			},
+		},
+	},
+});
