92dfe2610f12748be193e224b2f75187019146ab
hietashika <hietashika@gmail.com>
2025-04-04 15:10:55 +0900

    [chore] 🔧 Prettier, Biome, EditorConfig等の設定ファイルを追加・更新する - Prettier, Biome, EditorConfigを導入し、フォーマットとLint環境を整備する。 - VSCode設定で保存時の自動修正を有効化する。 - 関連する依存関係を`package-lock.json`に追加・更新する。

---
+++ .prettierrc.mjs
@@ -0,0 +1,24 @@
+import { createRequire } from "node:module";
+const require = createRequire(import.meta.url);
+
+/** @type {import("prettier").Config} */
+export default {
+	plugins: [
+		require.resolve("prettier-plugin-astro"),
+		require.resolve("prettier-plugin-organize-imports"),
+	],
+	overrides: [
+		{
+			files: "*.astro",
+			options: {
+				parser: "astro",
+			},
+		},
+	],
+	useTabs: true,
+	tabWidth: 2,
+	semi: true,
+	singleQuote: false,
+	trailingComma: "all",
+	printWidth: 80,
+};
