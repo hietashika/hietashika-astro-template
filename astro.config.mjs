b93438c7f2b855fc41c7538e81e021a59ca1f6c2
hietashika <hietashika@gmail.com>
2025-04-04 21:25:57 +0900

    [feat] 🔧 astro-iconパッケージと設定を追加する - アイコン機能をサポートするためのパッケージを導入し、SVG対応の設定を行う

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,17 @@ import mdx from "@astrojs/mdx";
 
 import playformCompress from "@playform/compress";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [mdx(), playformCompress()],
+	integrations: [
+		mdx(),
+		playformCompress(),
+		icon({
+			iconDir: "src/assets/icons",
+		}),
+	],
 	devToolbar: {
 		enabled: false,
 	},
@@ -16,11 +24,14 @@ export default defineConfig({
 			preprocessorOptions: {
 				scss: {
 					additionalData: `
-					@use "@styles/tools/_mixin.scss" as *;
-					@use "@styles/tools/_function.scss" as *;
-					@use "@styles/tools/_spacing.scss" as *;`,
+                    @use "@styles/tools/_mixin.scss" as *;
+                    @use "@styles/tools/_function.scss" as *;
+                    @use "@styles/tools/_spacing.scss" as *;`,
 				},
 			},
 		},
+	},
+	experimental: {
+		svg: true,
 	},
 });
